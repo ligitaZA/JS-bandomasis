69fff82262332afe6ccee909f2c909b8580a57c6
ligitaZA <ligitazaicevaite@gmail.com>
2022-12-30 15:39:06 +0200

    2 užduotis

--- task2/main.js
+++ task2/main.js
@@ -1,3 +1,17 @@
 /*
   Parašyti JS kodą, kuris skaičiuos kiek kartų buvo paspausta ant vieno arba kito elemento ekrane. (Ekrane turėti 2 nuotraukas ir laukelį po jomis, kuriame bus atvaizduojama kiek kartų buvo paspausta ant kurios nuotraukos)
 */
+
+function increaseCount(id) {
+  const countElement = document.getElementById(`${id}-count`);
+  let count = parseInt(countElement.innerHTML);
+  count++;
+  countElement.innerHTML = count;
+}
+document.getElementById('nuotrauka1').addEventListener('click', function() {
+  increaseCount('nuotrauka1');
+});
+
+document.getElementById('nuotrauka2').addEventListener('click', function() {
+  increaseCount('nuotrauka2');
+});
