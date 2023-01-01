aacd95cc96526a9d756bf7da25486974e8e2f660
ligitaZA <ligitazaicevaite@gmail.com>
2023-01-01 18:02:44 +0200

    4 užbaigta ir 10

--- task10/main.js
+++ task10/main.js
@@ -2,6 +2,42 @@
   Sutvarkykite 10 užduoties failų kodą taip, kad veiktų main.js parašytas kodas.
 */
 
+// let pirmas = vardoIlgis(asmuo1);
+// let antras = lytis(asmuo2);
+// let trecias = pilnametyste(asmuo3);
+// let ketvirtas = vardoIlgis(asmuo4);
+// let penktas = lytis(asmuo5);
+// let sesi = pilnametyste(asmuo6);
+
+// console.log(pirmas);
+// console.log(antras);
+// console.log(trecias);
+// console.log(ketvirtas);
+// console.log(penktas);
+// console.log(sesi);
+
+// Kintamieji su asmenų duomenimis
+let asmuo1 = { vardas: "Tadas", lytis: "vyras", pilnametyste: true };
+let asmuo2 = { vardas: "Gabrielė", lytis: "moteris", pilnametyste: false };
+let asmuo3 = { vardas: "Jonas", lytis: "vyras", pilnametyste: true };
+let asmuo4 = { vardas: "Paulina", lytis: "moteris", pilnametyste: true };
+let asmuo5 = { vardas: "Andrius", lytis: "vyras", pilnametyste: false };
+let asmuo6 = { vardas: "Aurelija", lytis: "moteris", pilnametyste: true };
+
+// fuunction
+function vardoIlgis(asmuo) {
+  return asmuo.vardas.length;
+}
+
+function lytis(asmuo) {
+  return asmuo.lytis;
+}
+
+function pilnametyste(asmuo) {
+  return asmuo.pilnametyste;
+}
+
+// result
 let pirmas = vardoIlgis(asmuo1);
 let antras = lytis(asmuo2);
 let trecias = pilnametyste(asmuo3);
@@ -15,3 +51,4 @@ console.log(trecias);
 console.log(ketvirtas);
 console.log(penktas);
 console.log(sesi);
+
--- task4/main.js
+++ task4/main.js
@@ -1,24 +1,36 @@
 /*
   Parašyti JS kodą, kuris, vartotojui atėjus į puslapį, iš data.json failo į ekraną išvestų filmus ir aktorius, kurie tuose filmuose vaidina. (duomenų yra didelis perteklius, jums jų visų naudoti nereikia. Tik filmų pavadinimai ir aktorių vardai+pavardės).
 */
-fetch('data.json')
-  .then(response => response.json())
-  .then(data => {
-    const movieList = document.createElement('ul');
-    data.movies && data.movies.forEach(movie => {
-      const movieTitle = movie.title;
-      const movieItem = document.createElement('li');
-      movieItem.textContent = movieTitle;
-      movieList.appendChild(movieItem);
+// Step 1: Define an async function that makes an HTTP request to the data.json file
+async function getData() {
+  try {
+    // Use the fetch API to make an HTTP request to the data.json file
+    const response = await fetch('data.json');
 
-      const actorList = document.createElement('ul');
-      movie.cast && movie.cast.forEach(actor => {
-        const actorName = `${actor.name.first} ${actor.name.last}`;
-        const actorItem = document.createElement('li');
-        actorItem.textContent = actorName;
-        actorList.appendChild(actorItem);
-      });
-      movieItem.appendChild(actorList);
+    // Convert the data to JSON format
+    const data = await response.json();
+
+    // Get the movie-list div element
+    const movieList = document.getElementById('movie-list');
+
+    // Loop through the movies array
+    data.movies.forEach(movie => {
+      // Create an li element
+      const movieItem = document.createElement('li');
+
+      // Create a text node with the movie title and actor name
+      const movieText = document.createTextNode(`${movie.title} - ${movie.castAndCrew.actors[0].name}`);
+
+      // Append the text node to the li element
+      movieItem.appendChild(movieText);
+
+      // Append the li element to the movie-list div
+      movieList.appendChild(movieItem);
     });
-    document.body.appendChild(movieList);
-  });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Step 2: Call the getData function when the page loads
+getData();
